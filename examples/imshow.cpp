8abedc826a5821b97c1ac7380a08c5d518d5a242
Trevor Keller <trevor.keller@nist.gov>
2019-06-07 14:31:44 -0400

    whitespace

--- examples/imshow.cpp
+++ examples/imshow.cpp
@@ -7,21 +7,21 @@ namespace plt = matplotlibcpp;
 
 int main()
 {
-    // Prepare data
-    int ncols = 500, nrows = 300;
-    std::vector<float> z(ncols * nrows);
-    for (int j=0; j<nrows; ++j) {
-        for (int i=0; i<ncols; ++i) {
-            z.at(ncols * j + i) = std::sin(std::hypot(i - ncols/2, j - nrows/2));
-        }
-    }
+	// Prepare data
+	int ncols = 500, nrows = 300;
+	std::vector<float> z(ncols * nrows);
+	for (int j=0; j<nrows; ++j) {
+		for (int i=0; i<ncols; ++i) {
+			z.at(ncols * j + i) = std::sin(std::hypot(i - ncols/2, j - nrows/2));
+		}
+	}
 
-    const float* zptr = &(z[0]);
-    const int colors = 1;
+	const float* zptr = &(z[0]);
+	const int colors = 1;
 
-    plt::title("My matrix");
-    plt::imshow(zptr, nrows, ncols, colors);
+	plt::title("My matrix");
+	plt::imshow(zptr, nrows, ncols, colors);
 
-    // Show plots
-    plt::show();
+	// Show plots
+	plt::show();
 }
--- examples/minimal.cpp
+++ examples/minimal.cpp
@@ -2,7 +2,8 @@
 
 namespace plt = matplotlibcpp;
 
-int main() {
-    plt::plot({1,3,2,4});
-    plt::show();
+int main()
+{
+	plt::plot({1,3,2,4});
+	plt::show();
 }
